d7b11b7148d230bd5f89a323c0cbcb5bd83e7654
Kusum <kusum.dhotre@gmail.com>
2024-05-13 12:47:41 +0530

    my first commit

---
+++ Datatype.java
@@ -0,0 +1,30 @@
+package Assignments;
+
+public class Datatype {
+
+	public static void main(String[] args) 
+	{
+		int a=15;
+		double b=70.90;
+		long c=67890677L;
+		float d=99.455F;
+		char e='A';
+		short f=3000;
+		byte g=4;
+		boolean h=false;
+		String s1="hello";
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(s1);		
+
+	}
+
+}
